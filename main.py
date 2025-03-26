f7bc0e3b29f8a7a18605f5d164d71e45a8d1dee1
Pushkal Vashist <118830976+Pushkaltoocool@users.noreply.github.com>
2025-03-26 17:11:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from twilio.rest import Client
+
+# Get secrets from environment variables
+API_KEY = os.getenv("API_KEY")
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("AUTH_TOKEN")
+
+LATITUDE = 1.290270
+LONGITUDE = 103.851959
+
+parameters = {
+    "lat": LATITUDE,
+    "lon": LONGITUDE,
+    "cnt": 4,
+    "appid": API_KEY
+}
+
+weather_codes = []
+response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+weather_data = response.json()["list"]
+
+for i in weather_data:
+    weather_codes.append(i["weather"][0]["id"])
+
+need_umbrella = any(int(code) < 700 for code in weather_codes)
+
+if need_umbrella:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body='It is going to rain today! Prepare an umbrella if you are going out ☂️',
+        to='whatsapp:+6590149200'
+    )
+    print(message.status)
